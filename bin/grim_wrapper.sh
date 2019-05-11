a422ded872ca2985fe53c5dc3c05f1227b2e4306
Michael Aquilina <michaelaquilina@gmail.com>
2019-05-11 14:42:41 +0100

    Small improvements to grim_wrapper

--- bin/grim_wrapper.sh
+++ bin/grim_wrapper.sh
@@ -1,7 +1,11 @@
 #! /usr/bin/bash
+
+# Exit on any failure
+set -e
+
 location="$HOME/Pictures/$(date "+%Y-%m-%d_%H-%M-%S").png"
 
-if [[ "$1" == "-d" ]]; then
+if [[ "$1" == "-g" ]]; then
     dimensions="$(slurp -d)"
     grim -g "$dimensions" "$location"
 else
