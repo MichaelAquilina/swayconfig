0ad5430e33f7799398d99645bfed700ed09c7aa1
Michael Aquilina <michaelaquilina@gmail.com>
2019-05-13 12:03:19 +0100

    [waybar] Better playerbar output

--- bin/playerbar.sh
+++ bin/playerbar.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/bash
 # Display appropriate metadata on waybar
 
-output="$(playerctl metadata -f '[{{status}}]  {{artist}} - {{album}} - {{title}}')"
+output="$(playerctl metadata -f '[{{status}}]  <b>{{title}}</b> by <i>{{artist}}</i> from <i>{{album}}</i>')"
 output="$(printf "$output" | sed 's/\[Playing\]//')"
 output="$(printf "$output" | sed 's/\[Paused\]//')"
 
