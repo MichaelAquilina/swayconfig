61fe96e4584b46217c8c4325705feab55f65764f
Michael Aquilina <michaelaquilina@gmail.com>
2019-09-29 21:59:35 +0100

    [lofi] general qol improvements

--- bin/lofi.sh
+++ bin/lofi.sh
@@ -7,8 +7,16 @@
 set -e
 
 PASSWORD_STORE="$HOME/.password-store"
-result="$(find "$PASSWORD_STORE" -name "*.gpg" -printf "%P\n" | fzf --layout reverse)"
+ALL_ITEMS="$(find "$PASSWORD_STORE" -name "*.gpg" -printf "%P\n")"
 
-pass "${result/.gpg/}" | head -1 | nohup wl-copy --paste-once
+result="$(echo "$ALL_ITEMS" | fzf --reverse --prompt "Password> ")"
 
-notify-send --app-name=pass "Password Store" "Copied $result to clipboard"
+pass "${result/.gpg/}" | head -1 | nohup wl-copy --paste-once 2>/dev/null
+
+# Show this in the background in case the GPG password prompt is shown
+echo "Unlock your password now"
+
+notify-send --app-name=pass \
+"Password Store" \
+"Copied <b>$result</b> to clipboard
+Password will only paste ONCE"
