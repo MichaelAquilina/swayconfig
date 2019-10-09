5026a02663cc621f146d71225d5f1ea96d763f41
Michael Aquilina <michaelaquilina@gmail.com>
2019-10-09 22:48:25 +0100

    [sway-power] split command over multiple lines

--- bin/sway-power.sh
+++ bin/sway-power.sh
@@ -1,2 +1,5 @@
 #! /usr/bin/bash
-swaynag -b 'logout' 'swaymsg exit' -b 'shutdown' 'systemctl poweroff' -b 'reboot' 'systemctl reboot' --type poweroff
+swaynag \
+    -b 'logout' 'swaymsg exit' \
+    -b 'shutdown' 'systemctl poweroff' \
+    -b 'reboot' 'systemctl reboot' --type poweroff
